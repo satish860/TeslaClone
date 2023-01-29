1b27e7612e9c5de45b4544d8a2d26b3cc373e95d
Satish Venkatakrishnan <satish860@gmail.com>
2023-01-29 10:38:47 +0530

    Car Item Added with the Assets

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import CarItem from './Components/CarItem/CarItem';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <CarItem/>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +18,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
+
 });
---
+++ Components/CarItem/CarItem.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
+
+function CarItem() {
+  return (
+    <View style={styles.carcontainer}>
+      <ImageBackground
+        style={styles.image}
+        source={require("../../assets/images/ModelS.jpeg")}
+      ></ImageBackground>
+      <Text style={styles.titles}>Model S</Text>
+      <Text style={styles.subTitle}>Starting at $64290</Text>
+    </View>
+  );
+}
+
+export default CarItem;
+
+const styles = StyleSheet.create({
+  carcontainer: {
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+  },
+
+  titles: {
+    marginTop: 40,
+    fontSize: 40,
+    fontWeight: "500",
+  },
+  subTitle: {
+    fontSize: 16,
+    color: "#5c5e62",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+    resizeMode: "contain",
+  },
+});
